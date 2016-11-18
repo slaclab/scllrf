947ca43aabb073f888af29b36c6bd709d52bccb9
gwbrown <gwbrown@slac.stanford.edu>
2016-11-18 11:43:31 -0800

    New IOCs to deploy at CMTF at JLab

--- display/startPRCScreens.cmd
+++ display/startPRCScreens.cmd
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-edm -eolc -x -m "P=PRC" -eolc scllrfPRCRegisters.edl &
-edm -eolc -x -m "P=PRC" -eolc PRCIQWaveforms.edl &
+edm -eolc -x -m "P=PRC1" -eolc scllrfPRCRegisters.edl &
+edm -eolc -x -m "P=PRC1" -eolc PRCIQWaveforms.edl &
