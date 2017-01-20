b2ea9866e92f5f258c6125981e5f7d45fc503125
gwbrown <gwbrown@slac.stanford.edu>
2017-01-20 13:56:28 -0800

    SLAC standard screens
    For the sake of collaborators, adding screens here that are part of
    SLAC's general collection of edm screens.

--- display/asynPRCRecord.cmd
+++ display/asynPRCRecord.cmd
@@ -3,6 +3,6 @@
 # Setup edm environment
 #source /reg/g/pcds/setup/epicsenv-3.14.12.sh
 
-edm -eolc -x -m "P=PRC1:CMTS,R=ASYN_IP,PORT=prcIP" asynRecord.edl &
-edm -eolc -x -m "P=PRC1:CMTS,R=ASYN_REG,PORT=prcReg" asynRecord.edl &
+edm -eolc -x -m "P=PRC1:,R=ASYN_IP,PORT=prcIP" asynRecord.edl &
+edm -eolc -x -m "P=PRC1:,R=ASYN_REG,PORT=prcReg" asynRecord.edl &
 
