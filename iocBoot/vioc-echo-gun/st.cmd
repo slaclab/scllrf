b8ef69e4bd61743c33863659efbd29c60081e56c
Garth Brown <gwbrown@slac.stanford.edu>
2019-02-12 12:39:44 -0800

    fixed text and macro problems

--- iocBoot/vioc-echo-gun/st.cmd
+++ iocBoot/vioc-echo-gun/st.cmd
@@ -5,49 +5,67 @@
 ## everywhere it appears in this file
 
 < envPaths
-
-# System Location:
-epicsEnvSet("AREA","B34")
+# PV name prefix parts in naming convention for Gun
+epicsEnvSet("DEVICE_TYPE", "GUN")
+epicsEnvSet("AREA","GUNB")
+epicsEnvSet("POSITION", "100")
+iocLogInit()
+iocLogPrefix("fac=${DEVICE_TYPE} proc=${IOC} ")
 # Hardware type [PRC, RFS, RES, INT]
 epicsEnvSet("CHASSIS_TYPE","GUNB")
 # Number within location and type: 1, 2, 3...
-epicsEnvSet("N","1")
-# PV prefix. SLAC standard is $(CHASSIS_TYPE):$(AREA):$(N):
-epicsEnvSet("P", "GUN$(N):$(AREA):")
+epicsEnvSet("CHASSIS_NAME","RFS1")
+epicsEnvSet("P", "$(DEVICE_TYPE):$(AREA):$(POSITION):$(CHASSIS_NAME):")
+epicsEnvSet("IOC_PV", "SIOC:$(AREA):RF01")
 # IP address of hardware
 #epicsEnvSet( FPGA_IP, "134.79.216.36")  # cdlx11
 epicsEnvSet( FPGA_IP, "127.0.0.1")  # if running on cdlx11
 #epicsEnvSet( FPGA_IP, "192.168.165.67")  # real PRC, can be used with loopback ports 50000 or 50002
 # UDP port number. 50006 for most, 7 for echo test interface, 3000 for cmoc
 epicsEnvSet( PORT, "7")
+# Command to set up python environment, only used in LinuxRT
+epicsEnvSet( GO_PY, "exit;") # for RT
+#epicsEnvSet( GO_PY, "") # for RHEL
+# RHEL needs the full path to the executable. LinuxRT doesn't, so PY_PATH can be blank
+epicsEnvSet( PY_PATH, "") # for RT
+#epicsEnvSet( PY_PATH, "/usr/local/lcls/package/python/python2.7.9/linux-x86_64/bin/") # for RHEL
+# Directory with python init script, and where it will be run from
+epicsEnvSet( PY_INIT_DIR, ".")
+# Bit file name, as relative path from the above directory
+epicsEnvSet( BIT_FILE, "../gun.bit")
+# The "chassis" register sets the type of chassis: 0:gun prc,1:gun rfs,2:buncher prc,3:buncher rfs
+epicsEnvSet(CHASSIS_REG, "1")
 # If this chassis has a subclass, by convention called extra, set its name
-# here so that scllrf$(CHASSIS_TYPE)$(EXTRA)Configure( "myReg","myIP") resolves correctly
-epicsEnvSet( EXTRA, "extra")
+# here so that scllrf$(CHASSIS_TYPE)$(EXTRA)Configure( "myReg","$(P)myIP") resolves correctly
+epicsEnvSet( EXTRA, "Extra")
 # This will work for the gun, which is not sc.
 epicsEnvSet( SC, "")
 
 < ../common/generalInit.cmd
-< iocBoot/common/regInterface.cmd
 # regInterface.cmd leaves us in $(TOP) directory
 
-####XXXX Turn on heavy logging for development
-# ======================================================================
-### Asyn Debugging #####################################################
-# ======================================================================
-## Asyn messages for DIGI_Serial16
-#asynSetTraceMask("myIP",-1,ASYN_TRACE_ERROR)
-asynSetTraceMask("myIP",-1,0xB)
-#asynSetTraceIOMask("myIP",-1,ASYN_TRACEIO_HEX) ASYN_TRACEIO_HEX = 4
-asynSetTraceIOMask("myIP",-1,4)
+< iocBoot/common/regInterface.cmd
+asynSetTraceMask("$(CHASSIS_NAME)IP",-1,1)
+asynSetTraceMask("$(CHASSIS_NAME)Reg",-1,1)
 
-# ======================================================================
-### Asyn Debugging #####################################################
-# ======================================================================
-## Asyn messages for DIGI_Serial16
-asynSetTraceMask("myReg",-1,0xB)
-#asynSetTraceIOMask("myReg",-1,ASYN_TRACEIO_HEX) ASYN_TRACEIO_HEX = 4
-asynSetTraceIOMask("myReg",-1,4)
-####XXXX End Turn on heavy logging for development
+epicsEnvSet("CHASSIS_NAME","RFS2")
+epicsEnvSet(CHASSIS_REG, "1")
+epicsEnvSet("P", "$(DEVICE_TYPE):$(AREA):$(POSITION):$(CHASSIS_NAME):")
+epicsEnvSet( FPGA_IP, "127.0.0.1")
+< iocBoot/common/regInterface.cmd
+asynSetTraceMask("$(CHASSIS_NAME)IP",-1,1)
+asynSetTraceMask("$(CHASSIS_NAME)Reg",-1,1)
+
+epicsEnvSet("CHASSIS_NAME","PRC")
+epicsEnvSet(CHASSIS_REG, "0")
+epicsEnvSet("P", "$(DEVICE_TYPE):$(AREA):$(POSITION):$(CHASSIS_NAME):")
+epicsEnvSet( FPGA_IP, "127.0.0.1")
+< iocBoot/common/regInterface.cmd
+asynSetTraceMask("$(CHASSIS_NAME)IP",-1,1)
+asynSetTraceMask("$(CHASSIS_NAME)Reg",-1,1)
+
+dbLoadRecords("db/GUNBExtra.db")
+dbLoadRecords("db/GunStatHistory.db")
 
 ##############################################################################
 # BEGIN: Load the record databases
@@ -55,22 +73,8 @@ asynSetTraceIOMask("myReg",-1,4)
 #< iocBoot/common/iocAdmin.cmd
 #< iocBoot/common/autoSaveConf.cmd
 
-# =====================================================================
-#Load Additional databases:
-# =====================================================================
-dbLoadRecords("db/GUNextra.db","P=$(P),PORT=myReg")
-#
 # END: Loading the record databases
 ########################################################################
-
-# =====================================================================
-# Channel Access Security:  
-# This is required if you use caPutLog.
-# Set access security file
-# Load common LCLS Access Configuration File
-#< ${ACF_INIT}
-#epicsThreadSleep(1.0)
-
 
 # =============================================================
 # Start EPICS IOC Process (i.e. all threads will start running)
@@ -104,14 +108,39 @@ iocInit()
 # An example of using the CEXP Shell:
 # cexpsh("-c",'printf("hello\n")')
 
-####XXXX Run a quick test, for dev only
-dbpr $(P)GET_HELL_R
-dbpf $(P)GET_HELL_R
+# Register initialization doesn't seem to play well with autosave or PINI, so init here.
 epicsThreadSleep(0.2)
-dbpr $(P)GET_HELL_R
-#dbpf $(CHASSIS_TYPE)$(N):RUN_STOP.HIGH 0.11
-#dbpf $(CHASSIS_TYPE)$(N):RUN_STOP 1
-epicsThreadSleep(0.2)
-asynSetTraceMask("myIP",-1,1)
-asynSetTraceMask("myReg",-1,1)
+epicsEnvSet("CHASSIS_NAME","RFS1")
+epicsEnvSet("P", "$(DEVICE_TYPE):$(AREA):$(POSITION):$(CHASSIS_NAME):")
+dbpf $(P)KEEP_W 0xFFFF
+dbpf $(P)DECAYKEEP_W 0xFFFF
+# The lower 16 bits of interlock status are diagnostic, the upper indicate a real trip
+dbpf $(P)INLK_STATUS_R.HIHI 0x10000
+dbpf $(P)POLL_PERIOD 0.1
+dbpf $(P)RUN_STOP 1
+seq amc78xxmon, "PREFIX=$(P)"
 
+epicsThreadSleep(0.2)
+epicsEnvSet("CHASSIS_NAME","RFS2")
+epicsEnvSet("P", "$(DEVICE_TYPE):$(AREA):$(POSITION):$(CHASSIS_NAME):")
+dbpf $(P)KEEP_W 0xFFFF
+dbpf $(P)DECAYKEEP_W 0xFFFF
+# The lower 16 bits of interlock status are diagnostic, the upper indicate a real trip
+dbpf $(P)INLK_STATUS_R.HIHI 0x10000
+dbpf $(P)POLL_PERIOD 0.1
+dbpf $(P)RUN_STOP 1
+seq amc78xxmon, "PREFIX=$(P)"
+
+epicsThreadSleep(0.2)
+epicsEnvSet("CHASSIS_NAME","PRC")
+epicsEnvSet("P", "$(DEVICE_TYPE):$(AREA):$(POSITION):$(CHASSIS_NAME):")
+dbpf $(P)KEEP_W 0x0FF0C3C3
+dbpf $(P)DECAYKEEP_W 0xC3C3
+# The lower 16 bits of interlock status are diagnostic, the upper indicate a real trip
+dbpf $(P)INLK_STATUS_R.HIHI 0x10000
+dbpf $(P)POLL_PERIOD 0.1
+dbpf $(P)RUN_STOP 1
+epicsThreadSleep(1)
+seq amc78xxmon, "PREFIX=$(P)"
+
+dbpf GUN:GUNB:100:WF_TIME_PER_POINT 4
