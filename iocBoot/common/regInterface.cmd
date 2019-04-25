e334e544c0f9c4aa0027bf1f06a0174cdc895ddf
Garth Brown <gwbrown@slac.stanford.edu>
2019-04-25 13:52:33 -0700

    Removing redundant "cd TOP". It\'s not needed, and causes problems when the build uses a relative path for TOP.

--- iocBoot/common/regInterface.cmd
+++ iocBoot/common/regInterface.cmd
@@ -2,7 +2,6 @@
 #The following line works for RHEL
 ####XXXX It's a pain to do this every time while testing
 system("${GO_PY=} cd ${PY_INIT_DIR}; ${PY_PATH=}python gun.py -a ${FPGA_IP} -b ${BIT_FILE}  -r -c ${CHASSIS_REG}")
-cd ${TOP}
 
 # =====================================================================
 # Set some facility specific MACROs for database instantiation below
