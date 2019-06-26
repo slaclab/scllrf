808141a83400755bab8afb30536545f64802a1a4
gwbrown <gwbrown@slac.stanford.edu>
2019-06-26 12:54:48 -0700

    Removing obsolete IOCs

--- iocBoot/vioc-echo-res/startup.cmd
+++
@@ -1,4 +0,0 @@
-#!../../bin/linuxRT-x86_64/scllrf
-< ./soft_pre_st.cmd
-< ./st.cmd
-< iocBoot/vioc-echo-res/soft_post_st.cmd
--- iocBoot/vioc-prc/startup.cmd
+++
@@ -1,4 +0,0 @@
-#!../../bin/linuxRT-x86_64/scllrf
-< ./soft_pre_st.cmd
-< ./st.cmd
-< iocBoot/vioc-echo/soft_post_st.cmd
