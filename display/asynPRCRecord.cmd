dfd9b0c1703610f3774890d88af6004c2c7bbfb5
gwbrown <gwbrown@slac.stanford.edu>
2019-06-26 17:23:28 -0700

    Many edl files moved to $EDM/llrf, or duplicates of existing screens.

--- display/asynPRCRecord.cmd
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-# Setup edm environment
-#source /reg/g/pcds/setup/epicsenv-3.14.12.sh
-
-edm -eolc -x -m "P=PRC1:,R=ASYN_IP,PORT=prcIP" asynRecord.edl &
-edm -eolc -x -m "P=PRC1:,R=ASYN_REG,PORT=prcReg" asynRecord.edl &
-
